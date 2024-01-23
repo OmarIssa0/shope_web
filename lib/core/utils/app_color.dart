a50e73f68bdbcac7e696825f85485a4c0944e621
OmarIssa0 <omar.issa.slwade@gmail.com>
2024-01-23 19:26:35 +0300

    app Styles and app color done

---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const kWhite = Color(0xffFFFFFF);
+  //
+  static const kGray900 = Color(0xff191C1F);
+  static const kGray800 = Color(0xff303639);
+  static const kGray700 = Color(0xff475156);
+  static const kGray600 = Color(0xff5F6C72);
+  static const kGray500 = Color(0xff77878F);
+  static const kGray400 = Color(0xff929FA5);
+  static const kGray300 = Color(0xffADB7BC);
+  static const kGray200 = Color(0xffC9CFD2);
+  static const kGray100 = Color(0xffE4E7E9);
+  static const kGray50 = Color(0xffF2F4F5);
+  //
+  static const kPrimary500 = Color(0xffFA8232);
+  static const kPrimary200 = Color(0xffFFCEAD);
+  static const kPrimary100 = Color(0xffFFE7D6);
+  static const kPrimary50 = Color(0xffFFF3EB);
+  //
+  static const kSecondary700= Color(0xff1B6392);
+  static const kSecondary600= Color(0xff2484C2);
+  static const kSecondary500= Color(0xff2DA5F3);
+  static const kSecondary100= Color(0xffD5EDFD);
+  static const kSecondary50= Color(0xffEAF6FE);
+
+  // 
+  static const kWarning500= Color(0xffEBC80C);
+  static const kWarning400= Color(0xffEFD33D);
+  static const kWarning300= Color(0xffF3DE6D);
+  static const kWarning200= Color(0xffF7E99E);
+  static const kWarning100= Color(0xffFDFAE7);
+  static const kWarning50= Color(0xffFBF4CE);
+  //
+  static const kSuccess500= Color(0xff2DB224);
+  static const kSuccess100= Color(0xffD5F0D3);
+  static const kSuccess50= Color(0xffEAF7E9);
+  //
+  static const kDanger700= Color(0xff8F3535);
+  static const kDanger600= Color(0xffBE4646);
+  static const kDanger500= Color(0xffEE5858);
+  static const kDanger100= Color(0xffFCDEDE);
+  static const kDanger50= Color(0xffFDEEEE);
+
+}
---
+++ lib/core/utils/app_style.dart
@@ -0,0 +1,284 @@
+import 'package:flutter/material.dart';
+import 'package:shope_web/core/utils/size_config.dart';
+
+abstract class AppStyles {
+  static TextStyle styleRegular14(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleMedium14(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleSemiBold14(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleMedium16(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleRegular16(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleSemiBold16(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleSemiBold12(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 12),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleBold14(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle styleRegular12(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 12),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleBold32(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 32),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle styleMedium18(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 18),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleRegular11Inter(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 11),
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleSemiBold14Inter(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleRegular18(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 18),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleSemiBold24(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 24),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleSemiBold20(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 20),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleSemiBold18(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 18),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleMedium12(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 12),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleBold16(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle styleSemiBold32(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 32),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleSemiBold40(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 40),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleSemiBold48(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 48),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleSemiBold22(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 22),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleRegular20(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 20),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleMedium11(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 11),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleRegular24(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 24),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleMedium20(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 20),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleSemiBold36(context, {required Color color}) {
+    return TextStyle(
+      color: color,
+      fontSize: getResponsiveFontSize(context, fontSize: 36),
+      fontFamily: 'PublicSans',
+      fontWeight: FontWeight.w600,
+    );
+  }
+}
+
+// ResponsiveFontSize
+double getResponsiveFontSize(context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+
+  double lowerLimit = fontSize * .8;
+  double upperLimit = fontSize * 1.2;
+
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(context) {
+  // var dispatcher = PlatformDispatcher.instance;
+  // var physicalWidth = dispatcher.views.first.physicalSize.width;
+  // var devicePixelRatio = dispatcher.views.first.devicePixelRatio;
+  // double width = physicalWidth / devicePixelRatio;
+
+  double width = MediaQuery.sizeOf(context).width;
+
+  if (width < SizeConfig.tablet) {
+    return width / 550;
+  } else if (width < SizeConfig.desktop) {
+    return width / 1000;
+  } else {
+    return width / 1920;
+  }
+}
